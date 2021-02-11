7b5f00300e6d22a1b16023b64d88934c1de4636a
LavaCode <kasper.zegel@gmail.com>
2021-02-11 20:51:10 +0100

    linked axios -- added async function

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hi!');
+console.log('Hi!'); //
+console.log(axios); //
+
+const searchButton = document.getElementById('searchButton');
+console.log(searchButton); //
+
+async function getCountryInfo() {
+    const country = "België";
+    const url = "https://restcountries.eu/#api-endpoints-language"
+    console.log(`Get country here`); //
+}
+
+searchButton.addEventListener("click", getCountryInfo);
