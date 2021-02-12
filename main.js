2d7de3db0f5344d277e930ae1d0d073e3301cefd
LavaCode <kasper.zegel@gmail.com>
2021-02-12 14:38:22 +0100

    task 6 done_working

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ searchButton.addEventListener("click", getCountryInfo);
 
 //API functionality - get data
 async function getCountryInfo() {
-    const country = "germany"; //get input from search 
+    const country = "belgie"; //get input from search 
     const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
     const response = await axios.get(url);
 
     const countryData = response.data[0]; //give country data
     const currencyData = countryData.currencies; // give country currency data
+    const languageData = countryData.languages;
+    console.log(languageData)
 
     try {
         for (const item of response.data) {
@@ -19,7 +21,8 @@ async function getCountryInfo() {
             } else {
                 console.log(`The capital is ${item.capital}`);
             }
-            console.log(formatCurrencies(currencyData));
+            console.log(getCurrencies(currencyData)); //import currencies
+            console.log(getLanguages(languageData)); //import languages
         }
     } catch (err) {
         // Handle Error Here
@@ -27,8 +30,8 @@ async function getCountryInfo() {
     }
 }
 
-//Function -- formatCurrencies, count type of currencies
-function formatCurrencies(currencies) {
+//Function -- getCurrencies, count type of currencies
+function getCurrencies(currencies) {
     let currencyString = ""; //
 
     for (let index = 0; index < currencies.length; index++) {
@@ -40,4 +43,24 @@ function formatCurrencies(currencies) {
         }
     }
     return currencyString;
+}
+
+//Function -- get languages
+function getLanguages(languages) {
+    let languageString = ""; //
+
+    for (let index = 0; index < languages.length; index++) {
+        const language = languages[index].name;
+        if (index === 0) {
+            languageString += `They speak ${language}`;
+            //eerste taal
+        } else if (index === (languages.length - 1)) {
+            languageString += ` and ${language}`;
+            //laatste taal
+        } else {
+            languageString += `, ${language}`;
+            //daartussen
+        }
+    }
+    return languageString;
 }
