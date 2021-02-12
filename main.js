dc4d78da812abf4b711b8bd3b88a00551e561d83
LavaCode <kasper.zegel@gmail.com>
2021-02-12 09:28:04 +0100

    first functionality; get functions work

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ const searchButton = document.getElementById('searchButton');
 console.log(searchButton); //
 
 async function getCountryInfo() {
-    const country = "België";
-    const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
-    console.log(`Get country here ${country}`); //
+    const country = "belgie"; //get input from search -- besides, test lower characters only
+    const response = await axios.get(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
+    try {
+        console.log(response.data); // werkt
+        for (const item of response.data) {
+            console.log(`${item.name} is situated in ${item.subregion}. It has a population of ${item.population} people.`);
+            console.log(`The capital is ${item.capital}`);
+        }
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
 }
 
 searchButton.addEventListener("click", getCountryInfo);
