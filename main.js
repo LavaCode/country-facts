cc3ffbed2b97f43a4149884d3573051290972240
LavaCode <kasper.zegel@gmail.com>
2021-02-12 14:28:28 +0100

    Finished task 4_works great

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-console.log('Hi!'); //
-console.log(axios); //
-
+//Code starts here
 const searchButton = document.getElementById('searchButton');
-console.log(searchButton); //
+searchButton.addEventListener("click", getCountryInfo);
 
+//API functionality - get data
 async function getCountryInfo() {
-    const country = "belgie"; //get input from search -- besides, test lower characters only
-    const response = await axios.get(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
+    const country = "germany"; //get input from search 
+    const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+    const response = await axios.get(url);
+
+    const countryData = response.data[0]; //give country data
+    const currencyData = countryData.currencies; // give country currency data
+
     try {
-        console.log(response.data); // werkt
         for (const item of response.data) {
             console.log(`${item.name} is situated in ${item.subregion}. It has a population of ${item.population} people.`);
-            console.log(`The capital is ${item.capital}`);
+            if (item.capital === "") {
+                console.log(`There is no capital city`);
+            } else {
+                console.log(`The capital is ${item.capital}`);
+            }
+            console.log(formatCurrencies(currencyData));
         }
     } catch (err) {
         // Handle Error Here
@@ -19,4 +27,17 @@ async function getCountryInfo() {
     }
 }
 
-searchButton.addEventListener("click", getCountryInfo);
+//Function -- formatCurrencies, count type of currencies
+function formatCurrencies(currencies) {
+    let currencyString = ""; //
+
+    for (let index = 0; index < currencies.length; index++) {
+        const currency = currencies[index].name;
+        if (index === 0) {
+            currencyString += `You can pay with ${currency}s`;
+        } else {
+            currencyString += ` and ${currency}s`;
+        }
+    }
+    return currencyString;
+}
